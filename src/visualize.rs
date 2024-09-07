3144dc41fb4acc787742306ea7073e1a9823e1dc
Jörg Singer <singer.joerg@gmx.de>
2024-09-07 13:01:46 +0200

    Removed superfluous visualize_with_source from Visualize trait

--- src/visualize.rs
+++ src/visualize.rs
@@ -9,12 +9,6 @@ pub trait Visualize {
     /// Writes the string representation of the nodes data.
     fn visualize(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result;
 
-    /// Writes the string representation of the nodes data with the help of the given source.
-    /// Is default implemented by calling `visualize`.
-    fn visualize_with_source(&self, f: &mut fmt::Formatter<'_>, _source: &str) -> fmt::Result {
-        self.visualize(f)
-    }
-
     /// When this method returns true the drawer can emphasize the node's string representation
     /// in an implementation dependent way, i.e. it can print it bold.
     fn emphasize(&self) -> bool {
